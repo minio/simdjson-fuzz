b116cdc491c822e3ab1e9bc4c8b34e39e08d2e87
Klaus Post <klauspost@gmail.com>
2019-12-04 07:05:27 -0800

    Don't -race

--- run.cmd
+++ run.cmd
@@ -5,7 +5,7 @@ del /Q %CORPUS%\crashers\*.*
 del /Q %CORPUS%\suppressions\*.*
 SET /a PROCS=%NUMBER_OF_PROCESSORS%/2
 
-go-fuzz-build -race -o=fuzz-build.zip -func=Fuzz .
+go-fuzz-build -o=fuzz-build.zip -func=Fuzz .
 
 :LOOP
 go run timeout.go -duration=1h go-fuzz -minimize=5s -bin=fuzz-build.zip -workdir=%CORPUS% -procs=%PROCS%
