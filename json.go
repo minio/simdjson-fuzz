f5903b340822695bf7ef7a5a6677d4870cd74775
Klaus Post <klauspost@gmail.com>
2019-12-05 05:44:00 -0800

    Add more content.

--- json.go
+++ json.go
@@ -7,11 +7,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/fwessels/simdjson-go"
+	"strings"
 )
 
 func Fuzz(data []byte) (score int) {
 	var dst map[string]interface{}
-	_, err := simdjson.Parse(data, nil)
+	var dstA []interface{}
+	pj, err := simdjson.Parse(data, nil)
 	jErr := json.Unmarshal(data, &dst)
 	if err != nil {
 		if jErr == nil {
@@ -27,14 +29,33 @@ func Fuzz(data []byte) (score int) {
 		return 0
 	}
 	if jErr != nil {
-		msg := fmt.Sprintf("no error reported, but json.Unmarshal reported: %v", jErr)
-		if false {
-			panic(msg)
+		if strings.Contains(jErr.Error(), "cannot unmarshal array into") {
+			jErr2 := json.Unmarshal(data, &dstA)
+			if jErr2 != nil {
+				msg := fmt.Sprintf("no error reported, but json.Unmarshal (Array) reported: %v", jErr2)
+				if false {
+					panic(msg)
+				} else {
+					fmt.Println(msg)
+				}
+			}
 		} else {
-			fmt.Println(msg)
+			msg := fmt.Sprintf("no error reported, but json.Unmarshal reported: %v", jErr)
+			if false {
+				panic(msg)
+			} else {
+				fmt.Println(msg)
+			}
 		}
 	}
-
+	// Check if we can convert back
+	i := pj.Iter()
+	if i.PeekNextTag() != simdjson.TagEnd {
+		_, err = i.MarshalJSON()
+		if err != nil {
+			panic(err)
+		}
+	}
 	// Do simple ND test.
 	d2 := append(make([]byte,0,len(data)*3+2), data...)
 	d2 = append(d2, '\n')
--- run.cmd
+++ run.cmd
@@ -8,5 +8,5 @@ SET /a PROCS=%NUMBER_OF_PROCESSORS%/2
 go-fuzz-build -o=fuzz-build.zip -func=Fuzz .
 
 :LOOP
-go run timeout.go -duration=1h go-fuzz -minimize=5s -bin=fuzz-build.zip -workdir=%CORPUS% -procs=%PROCS%
+go run timeout.go -duration=15m go-fuzz -minimize=5s -bin=fuzz-build.zip -workdir=%CORPUS% -procs=%PROCS%
 GOTO LOOP
