93344558e33c96bce99dd6a785da01802a9901fa
Klaus Post <klauspost@gmail.com>
2020-02-11 17:12:16 -0800

    corpus...

--- correctness.cmd
+++ correctness.cmd
@@ -1,12 +1,13 @@
 SET CORPUS=corpus
 SET GO111MODULE=off
-del /Q fuzz-build.zip
+SET ARCHIVE=fuzz-correct-build.zip
+del /Q %ARCHIVE%
 del /Q %CORPUS%\crashers\*.*
 del /Q %CORPUS%\suppressions\*.*
 SET /a PROCS=%NUMBER_OF_PROCESSORS%/4*3
 
-go-fuzz-build -o=fuzz-build.zip -func=FuzzCorrect .
+go-fuzz-build -o=%ARCHIVE% -func=FuzzCorrect .
 
 :LOOP
-go run timeout.go -duration=5m go-fuzz -minimize=5s -bin=fuzz-build.zip -workdir=%CORPUS% -procs=%PROCS%
+go run timeout.go -duration=5m go-fuzz -minimize=5s -bin=%ARCHIVE% -workdir=%CORPUS% -procs=%PROCS%
 GOTO LOOP
--- run.cmd
+++ run.cmd
@@ -1,12 +1,13 @@
 SET CORPUS=corpus
 SET GO111MODULE=off
-del /Q fuzz-build.zip
+SET ARCHIVE=fuzz-build.zip
+del /Q %ARCHIVE%
 del /Q %CORPUS%\crashers\*.*
 del /Q %CORPUS%\suppressions\*.*
 SET /a PROCS=%NUMBER_OF_PROCESSORS%
 
-go-fuzz-build -o=fuzz-build.zip -func=Fuzz .
+go-fuzz-build -o=%ARCHIVE% -func=Fuzz .
 
 :LOOP
-go run timeout.go -duration=5m go-fuzz -minimize=5s -bin=fuzz-build.zip -workdir=%CORPUS% -procs=%PROCS%
+go run timeout.go -duration=5m go-fuzz -minimize=5s -bin=%ARCHIVE% -workdir=%CORPUS% -procs=%PROCS%
 GOTO LOOP
