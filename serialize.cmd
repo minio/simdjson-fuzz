d0cfb332dfa95e69dde6b0192f4a198ae2fa9a4b
Klaus Post <klauspost@gmail.com>
2020-02-18 17:16:30 -0800

    Add serializer tests (#1)
    * Add serializer tests

---
+++ serialize.cmd
@@ -0,0 +1,13 @@
+SET CORPUS=corpus
+SET GO111MODULE=off
+SET ARCHIVE=fuzz-serial-build.zip
+del /Q %ARCHIVE%
+del /Q %CORPUS%\crashers\*.*
+del /Q %CORPUS%\suppressions\*.*
+SET /a PROCS=%NUMBER_OF_PROCESSORS%/4*3
+
+go-fuzz-build -o=%ARCHIVE% -func=FuzzSerialize .
+
+:LOOP
+go run timeout.go -duration=5m go-fuzz -minimize=5s -bin=%ARCHIVE% -workdir=%CORPUS% -procs=%PROCS%
+GOTO LOOP
